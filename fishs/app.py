b5c7c9194b30e76301c838ab52ca3e4e9c3ed139
[Humoyun1803] <[abduqodirovhumoyun3@gmail.com]>
2022-11-03 10:23:23 +0500

    commited

---
+++ fishs/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+st.title("Dengiz hayvonlarini klassifikatsiya qiluvchi model")
+
+#rasimni joylash
+file = st.file_uploader('Rasm yuklash', type=['png','jpg','gif','svg'])
+#model
+if file:
+    st.image(file)
+
+    #PIL convert
+    img = PILImage.create(file)
+
+
+    model = load_learner('fishs_model.pkl')
+    #predict
+
+    pred, pred_id, probs = model.predict(img)
+
+    st.success(f"Bashorat:{pred}")
+    st.info(f'Ehtimollik:{probs[pred_id]*100 : .1f} %')
+    fig = px.bar(x = probs*100, y = model.dls.vocab)
+    st.plotly_chart(fig)
+
+
---
+++ fishs/requirements.txt
@@ -0,0 +1,2 @@
+fastai
+plotly
